f9273b696e0e58812d620593714a484e2792683f
Seunghyun Ji <standingbehindnv@gmail.com>
2026-06-28 19:08:05 +0900

    autoscale removal

--- library/config/loader.py
+++ library/config/loader.py
@@ -417,7 +417,6 @@ def _count_training_image_paths(dataset_blueprint: "DatasetBlueprint") -> int:
 def generate_dataset_group_by_blueprint(
     dataset_group_blueprint: DatasetGroupBlueprint,
     target_res=None,
-    collapse_autoscale_tiers: bool = True,
 ) -> Tuple[DatasetGroup, Optional[DatasetGroup]]:
     datasets: List[DreamBoothDataset] = []
 
@@ -449,7 +448,6 @@ def generate_dataset_group_by_blueprint(
             subsets=subsets,
             **asdict(dataset_blueprint.params),
             is_training_dataset=True,
-            collapse_autoscale_tiers=collapse_autoscale_tiers,
         )
         datasets.append(dataset)
 
